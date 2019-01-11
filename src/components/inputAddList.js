8cbd22ce37580c5fddb06306c2ca1a929155ca52
993623069 <993623069@qq.com>
2019-01-11 16:18:19 +0800

    1321

--- src/components/inputAddList.js
+++ src/components/inputAddList.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import {connect} from "react-redux"
-let inputAddList=({dispatch})=>{
+let inputAddList=({dispatch,todos})=>{
+	debugger
     let submitText=function(e){
         e.preventDefault();
        dispatch({
  		type:"ADD_TODO",
  		id:0,   
- 		text:"text1"            
+ 		text:input.value       
  	})
     }
+    let input;
 return(
        <form onSubmit={(e)=>submitText(e)}>
-       	 <input type="text"/>
+       	 <input type="text" ref={node=>input=node}/>
        	 <button type="submit">Add list text</button>
        </form>
 	)
